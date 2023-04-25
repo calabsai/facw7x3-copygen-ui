9a93b8cdc79525913c1c3065148f75e09d746413
asparagusdelightful <rhen.k1@gmail.com>
2023-04-25 18:02:49 +0700

    Replaced local route with a route to the deployed backend

--- src/App.js
+++ src/App.js
@@ -146,8 +146,8 @@ const [animatedMessage, setAnimatedMessage] = useState('');
     data['selectedTemplate'] = selectedTemplate;
     data['languageModel'] = selectedLanguageModel;
     try {
-        // const response = await fetch('https://backend-api-acn7yotvaa-uc.a.run.app/generate', {
-        const response = await fetch('http://127.0.0.1:8080/generate', {
+        const response = await fetch('https://backend-api-acn7yotvaa-uc.a.run.app/generate', {
+        // const response = await fetch('http://127.0.0.1:8080/generate', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data),
